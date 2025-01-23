10e728c41e93041ce56ee8f2a56200d6cd020055
Bhawneet Singh <136002853+Bhawneet1@users.noreply.github.com>
2025-01-23 22:51:58 +0530

    Add files via upload

--- pivot_index.cpp
+++ pivot_index.cpp
@@ -1,28 +1,26 @@
-#include<iostream>
-#include<bits/stdc++.h>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <iostream>
+#include <numeric>
+#include <vector>
 using namespace std;
-int pivot(vector<int> arr){
-  int sum1=0;
-  int sum2=0;
-  for(int i=0;i<arr.size();i++){
-    for(int j=0;j<i;j++)
+int pivot(vector<int> &arr)
+{
+  int total_sum = accumulate(arr.begin(), arr.end(), 0);
+  int left_sum = 0;
+  for (int i = 0; i < arr.size(); ++i)
+  {
+    if (left_sum == total_sum - left_sum - arr[i])
     {
-      sum1+=arr[j];
-    }
-    for(int k=i+1;k<arr.size();k++)
-    {
-      sum2+=arr[k];
-    }
-    if(sum1==sum2){
       return i;
     }
-    
+    left_sum += arr[i];
   }
-  
+  return -1; // return -1 if no pivot index is found
 }
-int main(){
-  vector<int> arr={1,7,3,6,5,6};
-  cout<<pivot(arr);
+int main()
+{
+  vector<int> arr = {1, 7, 3, 6, 5, 6};
+  cout << pivot(arr);
   return 0;
 }
