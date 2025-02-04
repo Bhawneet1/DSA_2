03446368c12b5fdaa0c2269968035be7b3823b85
Bhawneet Singh <136002853+Bhawneet1@users.noreply.github.com>
2025-02-04 07:13:15 +0530

    Add files via upload

---
+++ Basic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int brr[5]={0};
+  for(int i=0;i<5;i++)
+  {
+    cout<<brr[i]<<endl;
+  }
+  int crr[]={0};
+  for(int i=0;i<5;i++)
+  {
+    cout<<crr[i]<<endl;
+  }
+}
